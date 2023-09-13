4a7ecd4851a74d32d20cb44a75e66f708304abcd
amansasidharan <118187955+amansasidharan@users.noreply.github.com>
2023-09-13 11:07:28 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -31,21 +31,9 @@ interface fifo_interface(input clk, rstn);
   bit o_alm_empty;
     input o_rddata;
   endclocking
-  
-/*  clocking pmon_cb @(posedge clk);
-    default input #1 output #1;
-    input i_wren;
-    input i_rden;
-    input i_wrdata;
-    input o_full;
-    input o_empty;
-     bit o_alm_full;
-  bit o_alm_empty;
-    input o_rddata;
-  endclocking*/
-  
+
   modport dri_mp (input clk, rstn, clocking dri_cb);
-   // modport amon_mp (input clk, rstn, clocking amon_cb);
+  
       modport mon_mp (input clk, rstn, clocking mon_cb);
     
 endinterface

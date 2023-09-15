e0db848d9194e9b1b767fc8bb2783f13fbc2477c
amansasidharan <118187955+amansasidharan@users.noreply.github.com>
2023-09-15 14:11:52 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -1,10 +1,10 @@
-class fifo_scoreboard extends uvm_scoreboard;
-  uvm_analysis_imp#(fifo_seq_item, fifo_scoreboard) item_got_export;
-  `uvm_component_utils(fifo_scoreboard)
-  int counter;
-  
-  function new(string name = "fifo_scoreboard", uvm_component parent);
-    super.new(name, parent);
+
+     class fifo_scoreboard extends uvm_scoreboard;
+       uvm_analysis_imp#(fifo_seq_item , fifo_scoreboard) item_got_export;
+       `uvm_component_utils(fifo_scoreboard)
+    int counter;
+       function new(string name = "fifo_scoreboard", uvm_component parent);
+    super.new(name , parent);
     item_got_export = new("item_got_export", this);
   endfunction
   
@@ -12,17 +12,17 @@ class fifo_scoreboard extends uvm_scoreboard;
     super.build_phase(phase);
   endfunction
   
-  bit[127:0] check_fifo[$];
+       bit [127:0] queue[$];
   
   function void write(input fifo_seq_item item_got);
     bit [127:0] examdata;
     if(item_got.i_wren == 'b1)begin
-       if(check_fifo.size()<1024) begin
-        counter =counter ++;
-      check_fifo.push_back(item_got.i_wrdata);
-      `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0h o_full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full), UVM_LOW);
+      if(queue.size()<1024) begin
+        counter =counter + 1;
+      queue.push_back(item_got.i_wrdata);
+      `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0h o_full: %0b  o_alm_full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full, item_got.o_alm_full), UVM_LOW);
     end
-else if(check_fifo.size() >=1020 && check_fifo.size() < 1024)
+      else if(queue.size() >=1020 && queue.size() < 1024)
         begin
             
           $display("The reference fifo is  Almost full");
@@ -30,11 +30,10 @@ else if(check_fifo.size() >=1020 && check_fifo.size() < 1024)
             begin
               $display("Dut output is matched with reference model");
             end
-
           else
             $display("No match");
-        counter =counter ++;
-      check_fifo.push_back(item_got.i_wrdata);
+        counter =counter + 1;
+      queue.push_back(item_got.i_wrdata);
       `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0h o_full: %0b  o_alm_full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full, item_got.o_alm_full), UVM_LOW);
         end
           else 
@@ -43,13 +42,12 @@ else if(check_fifo.size() >=1020 && check_fifo.size() < 1024)
         $display("The reference fifo is full");
             end
     end
-      
     else if (item_got.i_rden == 'b1)begin
- if(check_fifo.size() >= 'd1)begin
-        counter= counter--;
-        examdata = check_fifo.pop_front();
-   `uvm_info("Read Data", $sformatf("examdata: %0h o_rddata: %0h o_empty: %0b o_alm_empty: %0b", examdata, item_got.o_rddata, item_got.o_empty,item_got.o_alm_empty), UVM_LOW);
-   if(examdata == item_got.o_rddata)begin
+      if(queue.size() >= 'd1)begin
+        counter= counter-1;
+        examdata = queue.pop_front();
+        `uvm_info("Read Data", $sformatf("examdata: %0h o_rddata: %0h o_empty: %0b  o_alm_empty: %0b", examdata, item_got.o_rddata, item_got.o_empty , item_got.o_alm_empty), UVM_LOW);
+        if(examdata == item_got.o_rddata)begin
           $display("-------- 		Pass! 		--------");
         end
         else begin
@@ -57,33 +55,33 @@ else if(check_fifo.size() >=1020 && check_fifo.size() < 1024)
           $display("--------		Check empty	--------");
         end
       end
- else if(check_fifo.size() >0 && check_fifo.size()<3 )
- begin
+      else if(queue.size() >0 && queue.size()<3 )
+           begin
           
              $display("The reference fifo is  Almost empty");
              if(item_got.o_alm_empty==1)
             begin
               $display("Dut output is matched with reference model");
             end
-    else
+          else
             $display("No match");
-              counter= counter--;
-        examdata = check_fifo.pop_front();
-   `uvm_info("Read Data", $sformatf("examdata: %0h o_rddata: %0h o_empty: %0b  o_alm_empty: %0b", examdata, item_got.o_rddata, item_got.o_empty , item_got.o_alm_empty), UVM_LOW);
-   if(examdata == item_got.o_rddata)begin
+              counter= counter-1;
+        examdata = queue.pop_front();
+             `uvm_info("Read Data", $sformatf("examdata: %0h o_rddata: %0h o_empty: %0b  o_alm_empty: %0b", examdata, item_got.o_rddata, item_got.o_empty , item_got.o_alm_empty), UVM_LOW);
+           if(examdata == item_got.o_rddata)begin
           $display("-------- 		Pass! 		--------");
         end
         else begin
           $display("--------		Fail!		--------");
           $display("--------		Check empty	--------");
         end
-   
- end
-   else 
+           end
+              else 
             begin
               $display("The reference fifo is empty");
             end
+                
     end
   endfunction
 endclass
-        
+    

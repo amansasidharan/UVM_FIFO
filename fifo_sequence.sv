77e2d751d713943281333b86e8b9506340a045ab
amansasidharan <118187955+amansasidharan@users.noreply.github.com>
2023-09-15 14:13:21 +0530

    Update fifo_sequence.sv

--- fifo_sequence.sv
+++ fifo_sequence.sv
@@ -10,14 +10,14 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
     repeat(1024) begin
       req = fifo_seq_item::type_id::create("req");
       start_item(req);
-      assert(req.randomize() with {i_wren == 1 && i_rden==0;});
+      assert(req.randomize() with {i_wren == 1 ; i_rden==0;});
       finish_item(req);
     end
     `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
     repeat(1024) begin
       req = fifo_seq_item::type_id::create("req");
       start_item(req);
-      assert(req.randomize() with {i_rden == 1 && i_wren==0;});
+      assert(req.randomize() with {i_rden == 1 ; i_wren==0;});
       finish_item(req);
     end
     `uvm_info(get_type_name(), $sformatf("******** Generate 25 Random REQs ********"), UVM_LOW)
@@ -31,7 +31,25 @@ class fifo_sequence extends uvm_sequence#(fifo_seq_item);
     repeat(1024) begin
       req = fifo_seq_item::type_id::create("req");
       start_item(req);
-      assert(req.randomize() with {i_wren == 1 && i_rden==1;});
+      assert(req.randomize() with {i_wren == 1 ; i_rden==1;});
+      finish_item(req);
+    end
+     `uvm_info(get_type_name(), $sformatf("******** Generate no Read,Write REQs ********"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 0 ; i_wren==0;});
+      finish_item(req);
+    end
+     `uvm_info(get_type_name(), $sformatf("******** Generate 10 Alternative Read & Write REQs ********"), UVM_LOW)
+    repeat(1024) begin
+      req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 0 ; i_wren==1;});
+      finish_item(req);
+       req = fifo_seq_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 1 ; i_wren==0;});
       finish_item(req);
     end
   endtask
